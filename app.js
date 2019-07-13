26c7432d057e0da5bb7f2a775294ddac35ea6b1f
calisTONY97 <paulurbian@hotmail.com>
2019-07-13 15:57:13 -0500

    adding pusher config

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express();
 app.set('port', 3000 || process.env.PORT());
 
+// Set routes
+const RouterPolling = require('./routes/polling');
+
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +18,9 @@ app.use(express.urlencoded({extended: false}));
 
 // Enable Cors
 app.use(cors());
+
+// Get Router
+app.use('/polling', RouterPolling);
 
 // Server Listerning
 app.listen(app.get('port'), () => {
--- routes/polling.js
+++ routes/polling.js
@@ -1,1 +1,32 @@
-    
+var Pusher = require('pusher');
+const { Router } = require('express');
+const router = Router();
+
+
+// Initialiance pusher
+var pusher = new Pusher({
+  appId: '822753',
+  key: '549c2bdbf09152653457',
+  secret: 'dcad4d343bd2fd3a71b4',
+  cluster: 'us2',
+  encrypted: true
+});
+
+
+router.get('/', (req, res) => {
+    res.send('welcome from router');
+});
+
+router.post('/', (req, res) => {
+    pusher.trigger('os-poll', 'os-vote', {
+        points: 1,
+        os: req.body.os
+    });
+
+    return res.json({
+        success: true,
+        message: 'Thank you vote post!'
+    })
+});
+
+module.exports = router;
